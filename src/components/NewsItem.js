97066aaa919e399dbfe9a507741c6d40cc686b5e
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-03-03 21:57:18 +0530

    Fetching details

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div className="container ">
         <div class="card" style={{ width: "18rem" }}>
-          <img
-            src="https://www.reuters.com/resizer/ui17_X1wQ7jHxv8P3NnoSBB6kxo=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/477QWMRAGFPJLNDFDYJKGXA22Y.jpg"
-            class="card-img-top"
-            alt="..."
-          />
+          <img src={imageUrl} class="card-img-top" alt="..." />
           <div class="card-body">
             <h5 class="card-title">{title}</h5>
             <p class="card-text">{description}</p>
-            <a href="/" class="btn btn-primary">
-              Go somewhere
+            <a href="/" class="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>

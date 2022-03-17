f16b5c283167a9bfc5d6d9e4dc92980d1cb98e41
Ajaysdhillon <ajaysdhillon@gmail.com>
2022-03-17 22:14:38 +0530

    Adding source, author and date to news item component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,27 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="container ">
         <div class="card my-3" style={{ width: "18rem" }}>
+          <span
+            class="position-absolute top-0  translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
+          >
+            {source}
+          </span>
           <img src={imageUrl} class="card-img-top" alt="..." />
           <div class="card-body">
-            <h5 class="card-title">{title}</h5>
+            <h5 class="card-title">{title} </h5>
             <p class="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               href={newsUrl}
               target="_blank"
